c2f402e8678fc38edacc7208dccefc9ee3756f13
Chuck Dries <chuck@expensify.com>
2026-05-08 16:17:29 -0700

    Use expensify-common for reasonCode constants and plumb for all currently implemented reasons

--- src/libs/actions/User.ts
+++ src/libs/actions/User.ts
@@ -15,8 +15,8 @@ import type {
     GetStatementPDFParams,
     PusherPingParams,
     RequestContactMethodValidateCodeParams,
-    RevokeDeviceParams,
     ResendValidateCodeParams,
+    RevokeDeviceParams,
     SetContactMethodAsDefaultParams,
     SetNameValuePairParams,
     TogglePlatformMuteParams,
@@ -176,7 +176,7 @@ function closeAccount(reason: string) {
 /**
  * Resend a validation link to a given login
  */
-function resendValidateCode(login: string, reasonParams: ResendValidateCodeParams) {
+function resendValidateCode(reasonParams: ResendValidateCodeParams, login: string) {
     sessionResendValidateCode(reasonParams, login);
 }
 
